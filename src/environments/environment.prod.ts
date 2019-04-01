34cf826f5c08798691c05057459d32c41b971761
Miguel Gutierrez <miguelgutz95@gmail.com>
2019-03-31 22:38:54 -0700

    rem

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA5Ml3f3XOb6BFJJt30aqWDwMiV7tvdPBI",
-    authDomain: "grace-clean.firebaseapp.com",
-    databaseURL: "https://grace-clean.firebaseio.com",
-    projectId: "grace-clean",
-    storageBucket: "grace-clean.appspot.com",
-    messagingSenderId: "500472102794"
   }
 };

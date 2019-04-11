c65df61293f5be9d3f1d80bbc7f9e598f3ff2d33
Miguel Gutierrez <miguel.gutierrez@aa.com>
2019-04-11 16:41:59 -0700

    gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
+    apiKey: "AIzaSyA5Ml3f3XOb6BFJJt30aqWDwMiV7tvdPBI",
+    authDomain: "grace-clean.firebaseapp.com",
+    databaseURL: "https://grace-clean.firebaseio.com",
+    projectId: "grace-clean",
+    storageBucket: "grace-clean.appspot.com",
+    messagingSenderId: "500472102794"
   }
 };
 
